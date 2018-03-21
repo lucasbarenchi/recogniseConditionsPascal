5b3fb3fb202bc039305d9f2ac60340bedf770d89
Lucas Barenchi <lbarenchi@localhost.localdomain>
2018-03-21 18:28:36 -0300

    Plantilla finalizada

--- program1.py
+++ program1.py
@@ -2,4 +2,19 @@ import re
 import sys
 
 
-if "__name__" == __main__:
+if __name__ == '__main__':
+
+    # Leo parametros
+    archivo_entrada = sys.argv[1]
+    archivo_salida = sys.argv[2]
+
+    # Entrada
+    f = open(archivo_entrada, 'r')
+    datos = f.read()
+    f.close()
+
+    # Ejecucion y salida
+    salida = func(datos)
+    f = open(archivo_salida, 'w')
+    f.write(salida)
+    f.close()
